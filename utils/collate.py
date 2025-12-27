d4fdf5b4549f5d94b7058bbb729a62e52b1f98a2
kongkongsym <sym1458440989@gmail.com>
2025-12-27 17:28:08 +0000

    模型正在训练

--- utils/collate.py
+++ utils/collate.py
@@ -7,7 +7,7 @@ static methods.
 import torch
 import re
 import collections
-# from torch._six import string_classes
+# string_classes = (str, bytes)
 string_classes = (str, bytes)
 
 np_str_obj_array_pattern = re.compile(r'[SaUO]')
